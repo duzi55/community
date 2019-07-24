da43ec391a1721eb38b787ba20a8af8cd2879a25
duzi55 <1375744701@qq.com>
2019-07-24 10:08:24 +0800

    ok

---
+++ src/main/java/xyz/sdju/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package xyz.sdju.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @description:
+ * @author: duzi55
+ * @date: 2019-07-24 09:24
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
